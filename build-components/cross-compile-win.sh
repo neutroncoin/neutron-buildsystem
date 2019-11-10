95945549d09105acf399465a32bad7b5996a2f72
Chaozer <chaozer@swippcoin.com>
2019-11-10 20:18:49 +0100

    Clean up windows cross compilation script and make it work with latest MXE
    Because of some changes in locations, it required some small tweaks to
    the PATH env variable to find binaries for the build process and the
    lrelease script.
    
    This also changes the behavior to using "nproc" threads when compiling
    instead of using "nproc/2" threads, which was the previous behavior.

--- build-components/cross-compile-win.sh
+++ build-components/cross-compile-win.sh
@@ -30,8 +30,10 @@
 #
 # $1: Location of MXE
 # $2: Target (should be either i686-w64-mingw32.static, x86_64-w64-mingw32.static or x86_64-w64-minigw32.static.posix)
+#
 # --compile: Compile main executable
 # --compile-dependencies: Compile dependencies
+# --qmake: Run QMAKE script
 
 if [ "$0" = "$BASH_SOURCE" ]; then
 	if [ ! -d "$1" ]; then
@@ -48,12 +50,19 @@ if [ "$0" = "$BASH_SOURCE" ]; then
 	arg_target=$2
 fi
 
-PATH=$arg_mxe_path/usr/$arg_target/bin:$PATH
+PATH=$arg_mxe_path/usr/$arg_target/bin:$arg_mxe_path/usr/bin:$arg_mxe_path/usr/$arg_target/qt5/bin:$PATH
 MXE_INCLUDE_PATH=$arg_mxe_path/usr/$arg_target/include
 MXE_LIB_PATH=$arg_mxe_path/usr/$arg_target/lib
 
-win_qmake() {
-	$2-qt5 \
+if [[ "$@" =~ "--compile-dependencies" ]]; then
+	make MXE_TARGETS="$arg_target" boost
+	make MXE_TARGETS="$arg_target" qtbase
+	make MXE_TARGETS="$arg_target" qttools
+	make MXE_TARGETS="$arg_target" curl
+fi
+
+if [[ "$@" =~ "--qmake" ]]; then
+	$2-qmake-qt5 -Wnone \
 		BOOST_LIB_SUFFIX=-mt \
 		BOOST_THREAD_LIB_SUFFIX=_win32-mt \
 		BOOST_INCLUDE_PATH=$MXE_INCLUDE_PATH/boost \
@@ -65,41 +74,11 @@ win_qmake() {
 		MINIUPNPC_INCLUDE_PATH=$MXE_INCLUDE_PATH \
 		MINIUPNPC_LIB_PATH=$MXE_LIB_PATH \
 		CURL_LIB_PATH=$MXE_LIB_PATH \
-		QMAKE_LRELEASE=$1/usr/$2/qt5/bin/lrelease \
+		QMAKE_LRELEASE=$arg_mxe_path/usr/$arg_target/qt5/bin/lrelease \
 		MXE=1 USE_QRCODE=1 USE_UPNP=1 RELEASE=1 USE_BUILD_INFOP=1 \
 		neutron-qt.pro
-}
-
-win_leveldb() {
-	pushd src/leveldb
-	TARGET_OS=NATIVE_WINDOWS make -j$(($(nproc)/2)) out-static/libleveldb.a out-static/libmemenv.a \
-		CC=$1/usr/bin/$2-gcc \
-		CXX=$1/usr/bin/$2-g++
-	popd
-}
-
-win_sec256k1() {
-	pushd ../secp256k1
-	./autogen.sh
-	./configure --host=$1
-	make -j$(($(nproc)/2))
-	popd
-}
-
-win_main() {
-	make -j$(($(nproc)/2)) -f Makefile.Release
-}
-
-if [[ "$@" == "--compile-dependencies" ]]; then
-	make MXE_TARGETS="$arg_target" boost
-	make MXE_TARGETS="$arg_target" qtbase
-	make MXE_TARGETS="$arg_target" qttools
-	make MXE_TARGETS="$arg_target" curl
 fi
 
-if [[ "$@" == "--compile" ]]; then
-	win_qmake $arg_mxe_path $arg_target
-	win_leveldb $arg_mxe_path $arg_target
-	win_sec256k1 $arg_target
-	win_main
+if [[ "$@" =~ "--compile-main" ]]; then
+	make -j$(nproc) -f Makefile.Release
 fi
